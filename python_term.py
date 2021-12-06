81848cf8a821deffb3aa33710441f63449236b55
msk0624 <hsk55554@gmail.com>
2021-12-06 16:26:43 +0900

    finished first term project_titanic data set

--- python_term.py
+++ python_term.py
@@ -374,19 +374,58 @@ from tensorflow.keras.optimizers import Adam, SGD
 
 nn_model = Sequential()
 nn_model.add(Dense(32,activation='relu',input_shape=(14,)))
-nn_model.add(Dropout(0,2))
+nn_model.add(Dropout(0.2))
 nn_model.add(Dense(64,activation='relu'))
-nn_model.add(Dropout(0,2))
+nn_model.add(Dropout(0.2))
 nn_model.add(Dense(32,activation='relu'))
-nn_model.add(Dropout(0,2))
+nn_model.add(Dropout(0.2))
 nn_model.add(Dense(1,activation='sigmoid'))
 
 Loss = 'binary_crossentropy'
 nn_model.compile(loss=Loss,optimizer=Adam(),metrics=['accuracy'])
-print(nn_model.summary())
+#print(nn_model.summary())
 
+#history = nn_model.fit(X_tr,y_tr,
+#                    batch_size=64,
+#                    epochs=10,
+#                    validation_data=(X_vld, y_vld),
+#                    verbose=1)
 
+#hists = [history]
+#hist_df = pd.concat([pd.DataFrame(hist.history) for hist in hists], sort=True)
+#hist_df.index = np.arange(1, len(hist_df)+1)
+#fig , axs = plt.subplots(nrows=2,sharex=True,figsize=(16,10))
+#axs[0].plot(hist_df.val_accuracy, lw=5, label='Validation Accuracy')  #lw는 선의굵기
+#axs[0].plot(hist_df.accuracy, lw=5, label='Training Accuracy')
+#axs[0].set_ylabel('Accuracy')
+#axs[0].set_xlabel('Epoch')
+#axs[0].grid()
+#axs[0].legend(loc=0)
+#axs[1].plot(hist_df.val_loss, lw=5, label='Validation MLogLoss')
+#axs[1].plot(hist_df.loss, lw=5, label='Training MLogLoss')
+#axs[1].set_ylabel('MLogLoss')
+#axs[1].set_xlabel('Epoch')
+#axs[1].grid()
+#axs[1].legend(loc=0)
+#fig.savefig('hist.png') #이미지 파일로 저장
+#os.getcwd() #파이썬 작업 경로 확인(저장, 및 불러오는 곳)
+#print(plt.show())
 
+# #모델 예측 및 평가
 
+#submission = pd.read_csv('./gender_submission.csv')
+##print(submission.head())
 
+#prediction = model.predict(X_test)
+#submission['Survived'] = prediction
+##submission.to_csv('my_first_submission.csv', index=False) #예측 파일 생성 후 비교
 
+#prediction = nn_model.predict(X_test)
+#prediction = prediction > 0.5
+#prediction = prediction.astype(np.int)
+#prediction = prediction.T[0]
+#submission['Survived'] = prediction
+#print(prediction.shape)
+##submission.to_csv('my_first_submission.csv', index=False) #예측 파일 생성 후 비교
+
+## 이로써 처음 해본 titanic 생존 예측 텀과제를 마무리하겠씁니다!!!  
