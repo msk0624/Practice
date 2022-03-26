9fa588653d38143ba47c9d4d6ca365511949acb3
msk0624 <93578848+msk0624@users.noreply.github.com>
2022-03-26 17:52:09 +0900

    data visualize practice

---
+++ 0_practice/MLPractice.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0,6])
+ypoints = np.array([0,260])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+xpoints = np.array([1,9])
+ypoints = np.array([3,10])
+plt.plot(xpoints, ypoints,'v') #저 v가 점 형태인데 여러개가 있다.
+plt.show()
+
+xpoints = np.array([1,2,6,8])
+ypoints = np.array([3,8,1,10])
+plt.xlabel("PracticeX")
+plt.ylabel("PracticeY")
+
+plt.scatter(xpoints,ypoints) #점
+plt.plot(xpoints, ypoints) #선
+plt.bar(xpoints,ypoints) #바
+
+mylabels = ["x","y","z","w"]
+plt.pie(xpoints, labels=mylabels) #파이차트
+
+plt.show()
---
+++ asgmt_2_201821487.py
@@ -0,0 +1,14 @@
+from numpy import kron
+
+
+# 김혜성 / 201821487 / ghtn2638@ajou.ac.kr
+
+# 0. library and read csv
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('Salaries.csv')
+print(df)
+print(df.info())
+
+# 1. Generate texts with a couple of sentences that describe the data set based on your empirical insight and statistical approach. (4 points)
