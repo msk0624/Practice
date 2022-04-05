ed4919f861ed1faf5881c0a7b28962d2c6cba43c
msk0624 <93578848+msk0624@users.noreply.github.com>
2022-04-05 10:45:49 +0900

    no mean

---
+++ 0_practice/asgmt_1_201821487.py
@@ -0,0 +1,39 @@
+# Student name : 김혜성 
+# ID number : 201821487 
+# Email adress : ghtn2638@ajou.ac.kr 
+
+# library
+from math import sqrt
+import re
+
+# 1. convertCelsius
+def convertCelsius():
+    C_x = float(input("Enter a temperature degree in Celsius. "))
+    F_x = C_x * (9 / 5) + 32
+    print(F_x, "degrees Fahrenheit.")
+convertCelsius()
+    
+# 2. getLength
+def getLength():
+    Len_x = input("Enter a word or phrase. ")
+    print("The Length of the input is", len(Len_x), "characters.")
+getLength()
+
+# 3. quadraticRoot
+def quadraticRoot():
+    abc = input()
+    abc_list = re.findall("\d+", abc)
+    print(abc)
+    a = int(abc_list[0])
+    b = int(abc_list[1])
+    c = int(abc_list[2])
+    Xplus = (-b + sqrt(b**2-4*a*c))/2*a
+    Xminus = (-b - sqrt(b**2-4*a*c))/2*a
+    print("The solutions include {Xplus} and {Xminus}.".format(Xplus=Xplus,Xminus=Xminus))
+quadraticRoot()
+
+# 4. calcDistance
+def calcDistance(x1,y1,x2,y2):
+    d = sqrt((x2-x1)**2 + (y2-y1)**2)
+    return(float(d))
+calcDistance(-1,15,7,-3)
