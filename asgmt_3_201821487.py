0dc13d0b8cd11bd57e662c07d299ab9326bb4dc9
msk0624 <93578848+msk0624@users.noreply.github.com>
2022-04-16 23:40:07 +0900

    finished asgmt_3

--- asgmt_3_201821487.py
+++ asgmt_3_201821487.py
@@ -1,11 +1,15 @@
-# 0. libarary 
+## Student name : 김혜성 
+# ID number : 201821487 
+# Email adress : ghtn2638@ajou.ac.kr 
 
-from statistics import mean
+# 0. libarary 
+import re
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
 from paramiko import Agent
 import seaborn as sns
+from statistics import mean
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import roc_curve, auc
 from sklearn.metrics import classification_report
@@ -16,6 +20,9 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
+
+
+
 
 ## linear Regression
 
@@ -54,8 +61,6 @@ df_train.corr()
 sns.heatmap(df_train.corr()) #상관계수 표시
 plt.show()
 
-# 널값 처리 해야함.
-
 # A3. Convert the categorical variables into their dummies.
 df_train['raceeth'].unique()
 df_train['raceeth'].head()
@@ -78,6 +83,7 @@ X_train, X_vld, y_train, y_vld = train_test_split(df_train.drop('readingScore',a
 
 lmscore = LinearRegression()
 lmscore.fit(X_train,y_train)
+# If i training this lmscore model by using keras library with vld dataset, i think it will improve my lmscore model.
 
 print(lmscore.intercept_)
 # This is Beta zero
@@ -88,15 +94,14 @@ print(lmscore.intercept_)
 coeff_df = pd.DataFrame(lmscore.coef_,X.columns,columns=['Coefficient'])
 coeff_df
 # This is other Betas.
-print("The important indepedent variables are ")
+print("The higher the coefficient, the greater the influence on the dependent variable. So I think that important indepedent variables are grade, expectBachelors, read30minsAday, and raceeth_American Indian/Alaska Native, Black, Hispanic.")
 
 predictions = lmscore.predict(X_vld)
 plt.scatter(y_vld,predictions)
 plt.show()
 print("This model accuracy is {}%".format(lmscore.score(X_vld, y_vld)*100)) 
-# It's Accuracy is so bad...
-
-# Now doing with test.csv
+# It's Accuracy is so bad..., as i mentioned, it is necessary to doing training with keras..!
+# but now i'm doing with test.csv without vld training.
 df_test.info()
 
 X_test = df_train.drop('readingScore',axis=1)
@@ -129,6 +134,10 @@ print('MAE:', metrics.mean_absolute_error(y_test, predictions))
 print('MSE:', metrics.mean_squared_error(y_test, predictions))
 print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
 
+
+
+
+
 ## Logistic regression
 
 # B1. Deal with missing values and clean the data: e.g., remove whitespace at the end of the column names.
@@ -138,7 +147,8 @@ df_train.info()
 df_train.drop(['Unnamed: 13','Unnamed: 14','Unnamed: 15','Unnamed: 16'], axis=1, inplace=True)
 df_train.dropna(inplace=True)
 
-sns.heatmap(df_train.isnull(),yticklabels=False,cbar=False,cmap='viridis')
+# sns.heatmap(df_train.isnull(),yticklabels=False,cbar=False,cmap='viridis')
+# maybe run over code... it cause error..! becasue of many dataset..!
 
 # delete whitespace in columns end.
 cols = df_train.columns.tolist()
@@ -147,29 +157,104 @@ for entry in np.arange(0,len(cols),1):
 df_train.columns = cols
 df_train.columns
 
+df_train[['goal', 'pledged', 'usd pledged', 'backers']] = df_train[['goal', 'pledged', 'usd pledged', 'backers']].apply(pd.to_numeric, errors='coerce')
+# df_train.info()
+
 #Removing any random unhelpful ones. 
 df_train['state'].value_counts() < 100
-acc_states = ['failed', 'successful', 'live', 'undefined', 'suspended']
-df_train = df_train[df_train['state'].isin(acc_states)]
+acc_states = ['failed', 'successful', 'live', 'canceled', 'suspended'] 
+df_train = df_train[df_train['state'].isin(acc_states)] #일치하는 요소만 골라내는 함수
+df_train['state'].unique()
+
+print("Up to this point, I followed the website as a reference for efficient coding. If this wasn't the case, I'm really sorry, Professor.")
 
 # B2. Visualize data (using Seaborn and Matplotlib) to show the distributions of continuous variables and compositions of categorical variables based on your interests.
 
-df_train[['goal', 'pledged', 'usd pledged', 'backers']] = df_train[['goal', 'pledged', 'usd pledged', 'backers']].apply(pd.to_numeric, errors='coerce')
+plt.show()
+sns.countplot(x='main_category', data=df_train, palette='GnBu_d')
+sns.countplot(x='state', data=df_train, palette='GnBu_d')
+sns.countplot(x='country', data=df_train, palette='GnBu_d')
+sns.countplot(x='currency', data=df_train, palette='GnBu_d')
+sns.displot(data=df_train, x=df_train['goal'].dropna(),kde=True,color='darkred',bins=30)
+sns.displot(data=df_train, x=df_train['backers'].dropna(),kde=True,color='darkred',bins=30)
+# I think that datapreprocess in continuous value is necessary.
+
+plt.figure(figsize=(20,10))
+sns.countplot(x='main_category', data=df_train, hue='state')
+sns.countplot(x='country', data=df_train, hue='state')
+sns.countplot(x='currency', data=df_train, hue='state')
+
+
+# B3. Generate dummies of the categorical variables.
 df_train.info()
 
-# I interested in goal. because it means that The funding goal is the amount of money that a creator needs to complete their project."
+df_train['currency'].value_counts() < 6000
+df_train['country'].value_counts() < 6000
+df_train['currency'].unique()
+df_train['country'].unique()
+#I choose currency first.
+
+acc_currency = ['GBP','CAD','EUR','AUD']
+df_train = df_train[df_train['currency'].isin(acc_currency)] #일치하는 요소만 골라내는 함수
+df_train['currency'].unique()
+df_train=pd.get_dummies(data=df_train, columns=['currency'], dtype=float)
+df_train.columns
+
+#choose GBP as a reference catagory.
+df_train.drop('currency_GBP', axis=1, inplace=True)
+df_train.info()
+
+# B4. Do data processing to create a DataFrame containing only successful or failed projects.
+successfaildf = df_train[(df_train['state'] == 'successful') | (df_train['state'] == 'failed')]
+successfaildf['state']
+
+successfaildf['goal'][successfaildf['goal'] > 5000000]
+successfaildf = successfaildf[successfaildf['goal'] < 5000000]
+
+successfaildf['pledged'][successfaildf['pledged'] > 10000]
+successfaildf = successfaildf[(successfaildf['pledged'] < 10000) & (successfaildf['pledged'] > 0)]
+successfaildf.info()
+# Delete some highest values of pledged and goal..
+
+
+# B5. Build a logistic regression model by splitting the data into train and test sets.
+successfaildf['state'] = pd.get_dummies(data=successfaildf['state'], drop_first=True)
+successfaildf.info()
+successfaildf.corr()
+#X_variables = []
+#y_variable = []
+
+X = successfaildf[['goal', 'backers', 'currency_AUD', 'currency_CAD', 'currency_EUR']]
+# I want to add 'pledged' value. but some mystery reason make my logmodel acurracy 100... so i can't add 'pledged' value...
+# I think maybe if i put pledge value in my model. Then it cause overfitting problem. so i delete this value.
+y = successfaildf['state']
+X.info()
+y.info()
+
+
+# B6. Show the accuracy and precision metrics on the predictions
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=101)
+
+logmodel = LogisticRegression()
+logmodel.fit(X_train, y_train)
+predictions = logmodel.predict(X_test)
+probs = logmodel.predict_proba(X_test)
+
+print(confusion_matrix(y_test,predictions))
+print(classification_report(y_test, predictions))
+
+false_positive_rate, true_positive_rate, thresholds = roc_curve(y_test, probs[:,1])
+roc_auc = auc(false_positive_rate, true_positive_rate)
+
+plt.title('Receiver Operating Characteristic')
+plt.plot(false_positive_rate, true_positive_rate, 'b',
+label='AUC = %0.2f'% roc_auc)
+plt.legend(loc='lower right')
+plt.plot([0,1],[0,1],'r--')
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
 
 plt.show()
 
-sns.countplot(x='main_category', data=df_train, palette='RdBu_r')
-
-sns.countplot(x='Survived', hue='Sex', data=train)
-
-sns.displot(data=train, x=train['Age'].dropna(),kde=False,color='darkred',bins=30)
-
-sns.countplot(x='SibSp',data=train)
-
-plt.figure(figsize=(12,7))
-sns.boxplot(x='Pclass', y='Age', data=train, palette='winter')
-
-
+# B7. Print your own suggestions on how to improve the predictions with more accuracy and precision.
+print("Using Keras, it is expected that the accuracy will be higher if sufficient training is performed by splitting the train data into vld data before inserting the test data. Also, due to the problem of overfitting, the pledge variable could not be included here, but if this is also solved, it will be a good logistic model.")
